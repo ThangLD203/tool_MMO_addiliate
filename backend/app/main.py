d51af2346ef6c9f562a307f42441d4868ac7bbd6
mmediatelys <laiducthanghb.2003@gmail.com>
2026-05-14 23:50:28 +0700

    add BE module 1:v2

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(api_router, prefix="/api/config", tags=["config"])
+app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 def root():
